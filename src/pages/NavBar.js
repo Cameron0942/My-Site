5cf88c69c1e6d62fa3b85735574d572211b153fc
Cameron B <Cameron0942@users.noreply.github.com>
2023-06-24 03:09:22 -0700

    added functional hamburger menu

--- src/pages/NavBar.js
+++ src/pages/NavBar.js
@@ -6,6 +6,9 @@ import { Link } from 'react-scroll';
 //? MATERIAL UI
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import useMediaQuery from "@mui/material/useMediaQuery";
+import { IconButton, Menu, MenuItem } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+
 
 //? IMAGES
 import babby from '../assets/babby.png'
@@ -15,6 +18,7 @@ import babby from '../assets/babby.png'
 const theme = createTheme();
 
 const NavBar = () => {
+    const [anchorEl, setAnchorEl] = useState(null);
     const [isHovered, setIsHovered] = useState({
         home: false,
         skills: false,
@@ -27,6 +31,14 @@ const NavBar = () => {
     const isLg = useMediaQuery(theme.breakpoints.up('lg'));
     // const isMd = useMediaQuery(theme.breakpoints.up('md'));
     // const isSm = useMediaQuery(theme.breakpoints.up('sm'));
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+      };
+    
+      const handleClose = () => {
+        setAnchorEl(null);
+      };
 
     const handleMouseEnter = (id) => {
         
@@ -86,6 +98,70 @@ const NavBar = () => {
 
     return (
         <>
+            <div style={{ visibility: isLg ? 'hidden' : 'visible',top: 0, right: 0, position: 'fixed', zIndex: 999,}}>
+                <IconButton
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    aria-controls="menu"
+                    aria-haspopup="true"
+                    onClick={handleClick}
+                >
+                    <MenuIcon sx={{color: 'white', height: '2em', width: 'auto'}} />
+                </IconButton>
+                <Menu
+                    id="menu"
+                    anchorEl={anchorEl}
+                    keepMounted
+                    open={Boolean(anchorEl)}
+                    onClose={handleClose}
+                    PaperProps={{
+                        sx: {
+                          width: '12em',
+                          height: 'auto',
+                          backgroundColor: 'gray',
+                          backgroundImage: 'url(https://images.pexels.com/photos/1629236/pexels-photo-1629236.jpeg)',
+                          backgroundSize: 'cover',
+                        },
+                      }}
+                >
+                    <Link
+                        to="home"
+                        spy={true}
+                        smooth={true}
+                        duration={500}>
+                        <MenuItem onClick={handleClose} sx={{fontSize: '24px', color: 'whitesmoke', textShadow: '2px 2px black'}}>Home</MenuItem>
+                    </Link>
+                    <Link
+                        to="skills"
+                        spy={true}
+                        smooth={true}
+                        duration={500}>
+                        <MenuItem onClick={handleClose} sx={{fontSize: '24px', color: 'whitesmoke', borderTop: '1px solid black', textShadow: '2px 2px black'}}>Skills</MenuItem>
+                    </Link>
+                    <Link
+                        to="projects"
+                        spy={true}
+                        smooth={true}
+                        duration={500}>
+                        <MenuItem onClick={handleClose} sx={{fontSize: '24px', color: 'whitesmoke', borderTop: '1px solid black', textShadow: '2px 2px black'}}>Projects</MenuItem>
+                    </Link>
+                    <Link
+                        to="contact"
+                        spy={true}
+                        smooth={true}
+                        duration={500}>
+                        <MenuItem onClick={handleClose} sx={{fontSize: '24px', color: 'whitesmoke', borderTop: '1px solid black', textShadow: '2px 2px black'}}>Contact</MenuItem>
+                    </Link>
+                    <Link
+                        to="about-me"
+                        spy={true}
+                        smooth={true}
+                        duration={500}>
+                        <MenuItem onClick={handleClose} sx={{fontSize: '24px', color: 'whitesmoke', borderTop: '1px solid black', textShadow: '2px 2px black'}}>About Me</MenuItem>
+                    </Link>
+                </Menu>
+            </div>
         <ThemeProvider theme={theme}>
             <div style={{backgroundColor: '#050816', opacity: 1, display: isLg ? 'flex' : 'none', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center', paddingTop: 8}}>
                 <div style={{paddingLeft: 20}}>
