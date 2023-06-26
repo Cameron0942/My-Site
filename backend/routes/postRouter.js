2ec91182b34ef83fec25f9c452f12fe1e3c350b5
Cameron B <Cameron0942@users.noreply.github.com>
2023-06-25 20:28:27 -0700

    changed backedn route to start on /api

--- backend/routes/postRouter.js
+++ backend/routes/postRouter.js
@@ -8,12 +8,12 @@ const emailRoute = require("../controllers/emailRoute");
 
 postRouter.use(express.json());
 
-postRouter.post("/", (req, res, next) => {
+postRouter.post("/api", (req, res, next) => {
     console.log("POST request received");
     next(); // Call next() to pass the request to the next route handler
   });
 
 // postRouter.post("/", getColorPalette);
-postRouter.post("/", emailRoute);
+postRouter.post("/api", emailRoute);
 
 module.exports = postRouter;
